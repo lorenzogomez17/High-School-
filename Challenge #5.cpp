5be5bb119029332e4b7517964716529566bb1a60
lorenzogomez17 <72454940+lorenzogomez17@users.noreply.github.com>
2020-10-18 15:49:25 -0500

    Update Challenge #5.cpp

--- Challenge #5.cpp
+++ Challenge #5.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+    int main() {
+    int counter = 0;
+    int a, b, c, d, e, f, g, h, i, j = 0;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j; 
 
-int main() {
-   int a, b, c, d, e, f, g, h, i, j = 0;
-   cin >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j; 
-   cout << "Even Numbers:" << endl ;
-  
-   int array[10] = {a, b, c, d, e, f, g, h, i, j}; 
+    int arr[10] = {a, b, c, d, e, f, g, h, i, j}; 
+ 
    
-   int n=sizeof(array)/sizeof(array[0]);
+   int n=sizeof(arr)/sizeof(arr[0]);
+   int sep[sizeof(arr)/sizeof(0)];
    
    for (int i=0; i<n; i++) {
        
-       if (array[i]%2==0) { 
-
-           cout << array[i] <<endl;
-        }
-   } 
+       if (arr[i]%2==0) { 
+       sep[counter] = arr[i];
+        counter++;
+       }
+   }
+    for (i=0; i<10; i++) {
+        cout << sep[i] << endl;
+    }
    return 0; 
 }
    
